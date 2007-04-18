01d4f481c6cc5785a8a326e906e29709238787ee
Florian Ragwitz <rafl@debian.org>
2007-04-18 16:47:00 +0200

    Adjust the sign example to use non IO::Handle like data objects.

--- examples/sign.pl
+++ examples/sign.pl
@@ -7,13 +7,14 @@ use Crypt::GpgME;
 my $ctx = Crypt::GpgME->new;
 
 $ctx->set_passphrase_cb(sub {
+        warn "called";
         my ($ctx, $hook, $uid_hint, $passphrase_info, $prev_was_bad, $fd) = @_;
 
         return "foo";
 });
 
 my $plain = Crypt::GpgME::Data->new;
-print $plain 'test test test';
+$plain->write('test test test');
 
 my $signed = $ctx->sign($plain, 'clear');
 print while <$signed>;
