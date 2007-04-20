9c753f6e3ecf325f6173fe949570dae12ac7c1a1
Florian Ragwitz <rafl@debian.org>
2007-04-20 23:03:06 +0200

    Unbreak sign example.

--- examples/sign.pl
+++ examples/sign.pl
@@ -3,19 +3,14 @@
 use strict;
 use warnings;
 use Crypt::GpgME;
+use IO::Scalar;
 
 my $ctx = Crypt::GpgME->new;
 
-$ctx->set_passphrase_cb(sub {
-        my ($ctx, $hook, $uid_hint, $passphrase_info, $prev_was_bad, $fd) = @_;
+$ctx->set_passphrase_cb(sub { 'foo' });
 
-        return "foo";
-});
-
-my $plain = Crypt::GpgME::Data->new;
-$plain->write('test test test');
+my $plain = IO::Scalar->new(\q/test test test/);
 
 my $signed = $ctx->sign($plain, 'clear');
 
-$signed->read(my $buf, 1024);
-print $buf;
+print while <$signed>;
