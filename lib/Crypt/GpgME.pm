e9085bb878de8777b382be360a70bdce3503e34b
Florian Ragwitz <rafl@debian.org>
2007-04-24 23:49:26 +0200

    Basic docs for all functions.

--- lib/Crypt/GpgME.pm
+++ lib/Crypt/GpgME.pm
@@ -93,6 +93,152 @@ compiled against.
 
 Returns a new Crypt::GpgME instance. Throws an exception on error.
 
+=head2 card_edit
+
+    my $fh = $ctx->card_edit($key, $coderef);
+    my $fh = $ctx->card_edit($key, $coderef, $user_data);
+
+=head2 check_version
+
+    Crypt::GpgME->check_version;
+    Crypt::GpgME->check_version($version);
+
+=head2 delete
+
+    $ctx->delete($key);
+    $ctx->delete($key, $allow_secret);
+
+=head2 edit
+
+    my $fh = $ctx->edit($key, $coderef);
+    my $fh = $ctx->edit($key, $coderef, $user_data);
+
+=head2 engine_check_version
+
+    $ctx->engine_check_version($proto);
+    Crypt::GpgME->engine_check_version($proto);
+
+=head2 genkey
+
+    my ($result, $pubkey_fh, $seckey_fh) = $ctx->genkey($parms);
+
+=head2 get_armor
+
+    my $armor = $ctx->get_armor;
+
+=head2 get_engine_info
+
+    my $engine_info = $ctx->get_engine_info;
+    my $engine_info = Crypt::GpgME->get_engine_info;
+
+=head2 get_include_certs
+
+    my $include_certs = $ctx->get_include_certs;
+
+=head2 get_key
+
+    my $key = $ctx->get_key($fpr);
+    my $key = $ctx->get_key($fpr, $secret);
+
+=head2 get_keylist_mode
+
+    my $keylist_mode = $ctx->get_keylist_mode;
+
+=head2 get_protocol
+
+    my $protocol = $ctx->get_protocol;
+
+=head2 get_textmode
+
+    my $textmode = $ctx->get_protocol;
+
+=head2 keylist
+
+    my @results = $ctx->keylist($pattern);
+    my @results = $ctx->keylist($pattern, $secret_only);
+
+=head2 set_armor
+
+    $ctx->set_armor($armor);
+
+=head2 set_engine_info
+
+    $ctx->set_engine_info($proto, $file_name, $home_dir);
+    Crypt::GpgME->set_engine_info($proto, $file_name, $home_dir);
+
+=head2 set_include_certs
+
+    $ctx->set_include_certs;
+    $ctx->set_include_certs($nr_of_certs);
+
+=head2 set_keylist_mode
+
+    $ctx->set_keylist_mode;
+    $ctx->set_keylist_mode($keylist_mode);
+
+=head2 set_locale
+
+    $ctx->set_locale($category, $value);
+    Crypt::GpgME->set_locale($category, $value);
+
+=head2 set_passphrase_cb
+
+    $ctx->set_passphrase_cb($coderef);
+    $ctx->set_passphrase_cb($coderef, $user_data);
+
+=head2 set_progress_cb
+
+    $ctx->set_progress_cb($coderef);
+    $ctx->set_progress_cb($coderef, $user_data);
+
+=head2 set_protocol
+
+    $ctx->set_protocol;
+    $ctx->set_protocol($proto);
+
+=head2 set_textmode
+
+    $ctx->set_textmode($textmode);
+
+=head2 sig_notation_add
+
+    $ctx->sig_notation_add($name, $value);
+    $ctx->sig_notation_add($name, $value, $flags);
+
+=head2 sig_notation_clear
+
+    $ctx->sig_notation_clear;
+
+=head2 sig_notation_get
+
+    my @notation = $ctx->sig_notation_get;
+
+=head2 sign
+
+    my $fh = $ctx->sign($plain);
+    my $fh = $ctx->sign($plain, $mode);
+
+=head2 signers_add
+
+    $ctx->signers_add($key);
+
+=head2 signers_clear
+
+    $ctx->signers_clear;
+
+=head2 signers_enum
+
+    my $key = $ctx->signers_enum($seq);
+
+=head2 trustlist
+
+    my @trustlist = $ctx->trustlist($pattern, $maxlevel);
+
+=head2 verify
+
+    my ($result, $plain) = $ctx->verify($sig);
+    my $result = $ctx->verify($sig, $signed_text);
+
 =head1 AUTHOR
 
 Florian Ragwitz, C<< <rafl at debian.org> >>
