5127c5dfe7d9bffc998ba54c45790caf21ee75ca
Florian Ragwitz <rafl@debian.org>
2009-11-17 23:50:52 +0100

    Update copyright statements.

--- lib/Crypt/GpgME.pm
+++ lib/Crypt/GpgME.pm
@@ -283,7 +283,7 @@ L<http://search.cpan.org/dist/Crypt-GpgME>
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2007 Florian Ragwitz, all rights reserved.
+Copyright 2007-2009 Florian Ragwitz, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
